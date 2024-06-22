f929c6ba8f7a9ec5e711dd3966de3b4d3e3bc179
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-06-23 02:00:00 +0530

    port activation

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("runing");
+const exp = require("constants");
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const database = require("./config/database");
+
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// db connection
+database.connectDB();
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
